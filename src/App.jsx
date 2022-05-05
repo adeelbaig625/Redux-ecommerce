872ca1d759af4d426f0c98d48a9d0f7e7790fdc5
adeelbaig625 <adeelbaig625@gmail.com>
2022-05-05 11:10:42 -0700

    produt list added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 function App() {
- return <Home/>
+ return <ProductList/>
 }
 
 export default App;
