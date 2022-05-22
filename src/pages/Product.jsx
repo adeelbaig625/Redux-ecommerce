e19d145826af0480fd74686ddaaf748244299220
adeelbaig625 <adeelbaig625@gmail.com>
2022-05-22 03:22:34 -0700

    single product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -32,6 +32,38 @@ const Price=styled.span`
 font-weight: 100;
 font-size: 40px; 
 `
+const FilterContainer= styled.div`
+display: flex;
+width: 50%;
+margin: 30px 0px;
+justify-content: space-between;
+`
+const Filter = styled.div`
+display: flex;
+align-items: center;
+`
+const FilterTitle = styled.span`
+font-size: 20px;
+font-weight: 200;
+`
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props=>props.color};
+  margin: 0px 5px;
+  cursor:pointer;
+`
+const FilterSize = styled.select`
+margin-left: 10px;
+padding: 5px;
+`
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div``
+const AmountContainer = styled.div``
+const Amount = styled.option``
+const Button = styled.button``
 
 function Product() {
   return (
@@ -48,6 +80,35 @@ function Product() {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, expedita sed. Adipisci mollitia quia quas maiores delectus aliquid doloribus ad, sunt illo velit soluta fugiat eius in sed aperiam debitis.
             </Desc>
             <Price>$20</Price>
+            <FilterContainer>
+              <Filter>
+                <FilterTitle>Color</FilterTitle>
+                <FilterColor color="black"/>
+
+                <FilterColor color="darkblue"/>
+                <FilterColor color="gray"/>
+                
+              </Filter>
+              <Filter>
+                <FilterTitle>Size</FilterTitle>
+                <FilterSize>
+                  <FilterSizeOption>XS</FilterSizeOption>
+                  <FilterSizeOption>S</FilterSizeOption>
+                  <FilterSizeOption>M</FilterSizeOption>
+                  <FilterSizeOption>L</FilterSizeOption>
+                  <FilterSizeOption>XL</FilterSizeOption>
+                </FilterSize>
+                
+              </Filter>
+            </FilterContainer>
+      <AddContainer>
+        <AmountContainer>
+       
+          <Amount>1</Amount>
+      
+        </AmountContainer>
+        <Button>ADD TO CART</Button>
+      </AddContainer>
         </InfoContainer>
        </Wrapper>
        <Newsletter/>
