334280999e021b21fd84eff47209a168ac4d4a05
adeelbaig625 <adeelbaig625@gmail.com>
2022-06-12 03:01:14 -0700

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,40 @@ import Home from './pages/Home';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Cart from './pages/Cart';
-
+import Login from './pages/Login'
+import Register from './pages/Register'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 function App() {
- return <Cart/>
+ return <Router>
+     <Switch>
+         <Route exact path="/">
+            <Home/>
+         </Route>
+         <Route path="/cart">
+            <Cart/>
+         </Route>
+         <Route path="/products/:category">
+            <ProductList/>
+         </Route>
+         <Route path="/products/:category">
+            <Product/>
+         </Route>
+         <Route path="/products/:id">
+            <Product/>
+         </Route>
+         <Route path="/login">
+            <Login/>
+         </Route>
+         <Route path="/register">
+            <Register/>
+         </Route>
+     </Switch>
+ </Router>
 }
 
 export default App;
