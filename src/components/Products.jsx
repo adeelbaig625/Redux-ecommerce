ac10ce72cb95c44980b05360034bb1c01d15c948
adeelbaig625 <adeelbaig625@gmail.com>
2022-06-14 15:06:26 -0700

    filter not completed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import {popularProducts} from '../data'
 import Product from './Product'
+import axios from 'axios'
 const Container=styled.div`
    display: flex;
    padding: 20px;
    flex-wrap: wrap;
 `
 const Products = ({cat,filters,sort}) => {
+  const [products,setProducts]=React.useState([])
+  const [filteredProducts,setFilteredProducts]=React.useState([]);
+  React.useEffect(()=>{
+const getProducts=async ()=>
+{
+  try{
+    const res=await axios.get("http://localhost:3000/api/products")
+    console.log(res)
+}catch(err)
+  {
+    console.log(err)
+  }
+  getProducts()
+}
+  },[cat])
+  
   return (
     <Container>
         {popularProducts.map(item=>(
